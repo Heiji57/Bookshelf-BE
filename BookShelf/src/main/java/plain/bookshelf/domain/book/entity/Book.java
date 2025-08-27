2ee58422e855025d88d831ec7e8aaf86ea9ddae5
heiji57 <kjone0kjone0591@gmail.com>
2025-08-27 17:05:58 +0900

    feat: Implement book - 책 entity 추가

--- BookShelf/src/main/java/plain/bookshelf/domain/book/entity/Book.java
+++ BookShelf/src/main/java/plain/bookshelf/domain/book/entity/Book.java
@@ -1,7 +1,19 @@
 package plain.bookshelf.domain.book.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
 
-
+@Entity
+@Table(name = "book")
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Book {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
+    private Long bookId;
+
+    @Column(name = "rental_status")
+    private boolean rentalStatus = false;
 }

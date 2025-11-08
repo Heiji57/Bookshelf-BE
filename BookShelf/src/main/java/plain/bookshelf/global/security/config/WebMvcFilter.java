5b3de3bbafea92bbf52e27e5ed29aa4e19adc3f5
Heiji57 <kjone0kjone0591@gmail.com>
2025-11-08 16:59:50 +0900

    fix: authenticationFilter - authenticationFilter 수정

--- BookShelf/src/main/java/plain/bookshelf/global/security/config/WebMvcFilter.java
+++ BookShelf/src/main/java/plain/bookshelf/global/security/config/WebMvcFilter.java
@@ -12,7 +12,7 @@ public class WebMvcFilter implements WebMvcConfigurer {
         registry.addMapping("/**") // 모든 api 요청에 cors 규칙을 적용하겠다는 뜻 그냥 /** 쓰면 될 듯
                 .allowedOrigins("http://localhost:8080", "http://localhost:9200", "http://13.125.65.240:8080", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                .allowedHeaders("*")
+                .allowedHeaders("Authorization", "Cache-Control", "Content-Type")
                 .allowCredentials(true) // JWT 인증 시 필수 Authorization 헤더가 (JWT)를 포함하거나 쿠키를 주고받을 수 있도록 허용
 
                 // Preflight 요청 결과(OPTIONS 메서드를 통한 사전 통신)를 3600초(1시간) 동안 캐시하여, 매 요청마다 Preflight 통신을 하는 오버헤드를 줄임.
